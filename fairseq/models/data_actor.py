8b4ce6b1a7306a95e1799b58ae73bb606e7f3343
xwang29 <cindyxinyiwang@gmail.com>
2019-12-02 11:00:21 -0500

    update data actor

--- fairseq/models/data_actor.py
+++ fairseq/models/data_actor.py
@@ -18,7 +18,7 @@ class BaseActor(torch.nn.Module):
     return logits
 
 class LanguageActor(torch.nn.Module):
-  def __init__(self, args, lan_size):
+  def __init__(self, args, lan_size, optimize_emb=True):
     super(LanguageActor, self).__init__()
     self.args = args
     self.lan_size = lan_size
@@ -26,8 +26,18 @@ class LanguageActor(torch.nn.Module):
     if self.args.lan_embed_dim is None:
         lan_embed_dim = embed_dim 
     else:
-        lan_embed_dim = self.args.lan_embed_dim 
-    self.lan_emb = Embedding(self.lan_size, lan_embed_dim, None)
+        lan_embed_dim = self.args.lan_embed_dim
+    if self.args.embedding_file:
+        self.lan_emb, fixed_embed_dim, embed_num = FixedEmbedding(self.args.embedding_file)
+        assert embed_num == self.lan_size
+        self.rand_lan_emb = Embedding(self.lan_size, lan_embed_dim-fixed_embed_dim)
+        if not optimize_emb:
+            self.lan_emb.weight.requires_grad = False
+            self.rand_lan_emb.weight.requires_grad = False
+    else:
+        self.lan_emb = Embedding(self.lan_size, lan_embed_dim, None)
+        if not optimize_emb:
+            self.lan_emb.weight.requires_grad = False
     # init
     self.w = torch.nn.Linear(lan_embed_dim, embed_dim)
     self.project_out = torch.nn.Linear(embed_dim, 1)
@@ -39,7 +49,12 @@ class LanguageActor(torch.nn.Module):
   def forward(self, feature):
     # input feature is lan id
     # feature: [1, 1]
-    emb = self.lan_emb(feature)
+    if self.args.embedding_file:
+        emb_1 = self.lan_emb(feature)
+        emb_2 = self.rand_lan_emb(feature)
+        emb = torch.cat([emb_1, emb_2], dim=-1)
+    else:
+        emb = self.lan_emb(feature)
     x = self.w(emb)
     logits = self.project_out(x).squeeze(2)
     return logits
@@ -97,6 +112,20 @@ class AveEmbActor(torch.nn.Module):
         elif self.out_score_type == 'exp':
             score = torch.exp(self.project_out(inp))
         return score 
+
+def FixedEmbedding(embedding_file):
+    embedding_data = []
+    with open(embedding_file, 'r') as myfile:
+        for line in myfile:
+            toks = line.split()
+            embedding_data.append([float(t) for t in toks])
+    embedding_data = torch.FloatTensor(embedding_data)
+    embedding_dim = embedding_data.size(1)
+    num_embeddings = embedding_data.size(0)
+    m = torch.nn.Embedding(num_embeddings, embedding_dim)
+    m.weight = torch.nn.Parameter(embedding_data)
+    return m, embedding_dim, num_embeddings
+
 
 def Embedding(num_embeddings, embedding_dim, padding_idx=None):
     m = torch.nn.Embedding(num_embeddings, embedding_dim, padding_idx=padding_idx)
