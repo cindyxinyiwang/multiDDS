d69bf425fc8a1e451b431f4f12b423c4d20a0b4b
cindyxinyiwang <xwang29@nd.edu>
2020-02-07 12:40:07 -0500

    merge

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -1310,12 +1310,7 @@ class Trainer(object):
                 elif self.args.out_score_type == "proj_word_score":
                     #loss = (-torch.log(1e-10+data_actor_out[idx]) * (reward.data - self.baseline).masked_fill_(pad_masks[idx], 0.)).sum() * (1-self.args.data_loss_lambda) + (-data_actor_proj_out[idx].masked_fill_(pad_masks[idx], 0.)).sum() * self.args.data_loss_lambda
                     #loss = (-torch.log(1e-10+data_actor_out[idx]) * (reward.data - self.baseline)) * (1-self.args.data_loss_lambda) + (-data_actor_proj_out[idx]) * self.args.data_loss_lambda
-<<<<<<< HEAD
                     loss = -data_actor_out[idx] * (reward.data - self.baseline)
-=======
-                    loss = (-data_actor_out[idx] * (reward.data - self.baseline)) * (1-self.args.data_loss_lambda) + (-data_actor_proj_out[idx]) * self.args.data_loss_lambda
-                loss.div_(loss_data.size(0))
->>>>>>> c75209695d11456078aa949d059c75622bcaa38b
                 loss = loss[~pad_masks[idx]]
                 loss.div_(tgt_len)
                 loss.sum().backward()
