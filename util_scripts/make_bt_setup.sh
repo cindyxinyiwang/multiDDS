a1df2c01c3796b72d2918ba470888388b22bf7b3
cindyxinyiwang <xwang29@nd.edu>
2020-03-21 20:59:07 -0400

    scripts

--- util_scripts/make_bt_setup.sh
+++ util_scripts/make_bt_setup.sh
@@ -6,16 +6,17 @@ script_base_dir=job_scripts/bt_iwslt_ende/ende_
 template_dir=util_scripts/bt_dds_templates/
 SEED=2
 
-for dis in 0.90 ; do
+for dis in 0.99 0.95; do
     for lr in 1e-9; do
     for gradn in 5; do
-    for optim in SGD Adam; do
-        for temp in dds_m_dbase_nwords dds_m_dbase; do
-            script_name="$script_base_dir""$temp"_dis"$dis"_lr"$lr"_gradn"$gradn"_optim"$optim".sh
-            dirname="$cp_base_dir""$temp"_dis"$dis"_lr"$lr"_gradn"$gradn"_optim"$optim"
+    for optim in SGD ; do
+    for seed in 2 3 4 ; do
+        for temp in dds_m_dis; do
+            script_name="$script_base_dir""$temp"_dis"$dis"_lr"$lr"_gradn"$gradn"_optim"$optim"_se"$seed".sh
+            dirname="$cp_base_dir""$temp"_dis"$dis"_lr"$lr"_gradn"$gradn"_optim"$optim"_se"$seed"
             echo $script_name
             echo $dirname
-            sed "s@DIR_NAME@$dirname@g; s@SEED@$SEED@g; s@LR@$lr@g; s@DIS@$dis@g; s@GRADN@$gradn@g; s@OPTIM@$optim@g" < $template_dir/$temp > $script_name 
+            sed "s@DIR_NAME@$dirname@g; s@SEED@$seed@g; s@LR@$lr@g; s@DIS@$dis@g; s@GRADN@$gradn@g; s@OPTIM@$optim@g" < $template_dir/$temp > $script_name 
             #sed "s/SEED/$SEED/g; s/LR/$lr/g; s/DIS/$dis/g" < $template_dir/$temp > $script_name 
             mkdir -p $dirname
             cp $bt_model $dirname/checkpoint_last.pt
@@ -23,5 +24,32 @@ for dis in 0.90 ; do
     done
     done
     done
+    done
 done
 
+#bt_model=checkpoints/bt_iwslt_deen_v2/ende/checkpoint_best.pt
+#cp_base_dir=checkpoints/bt_iwslt_deen_v2/
+#script_base_dir=job_scripts/bt_iwslt_deen_v2/
+#
+#template_dir=util_scripts/bt_dds_templates/
+#SEED=2
+#
+#for dis in 0.99; do
+#    for lr in 1e-9 1e-10; do
+#    for gradn in 0; do
+#    for optim in SGD Adam; do
+#        for temp in  deen_dds_m_dis; do
+#            script_name="$script_base_dir""$temp"_dis"$dis"_lr"$lr"_gradn"$gradn"_optim"$optim".sh
+#            dirname="$cp_base_dir""$temp"_dis"$dis"_lr"$lr"_gradn"$gradn"_optim"$optim"
+#            echo $script_name
+#            echo $dirname
+#            sed "s@DIR_NAME@$dirname@g; s@SEED@$SEED@g; s@LR@$lr@g; s@DIS@$dis@g; s@GRADN@$gradn@g; s@OPTIM@$optim@g" < $template_dir/$temp > $script_name 
+#            #sed "s/SEED/$SEED/g; s/LR/$lr/g; s/DIS/$dis/g" < $template_dir/$temp > $script_name 
+#            mkdir -p $dirname
+#            cp $bt_model $dirname/checkpoint_last.pt
+#        done
+#    done
+#    done
+#    done
+#done
+#
