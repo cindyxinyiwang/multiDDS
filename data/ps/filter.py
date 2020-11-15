2acdf0cc0648bab39bb0f7f6d997b0b9a665a741
Steven Tan <wtan12@jhu.edu>
2020-11-15 13:37:59 -0500

    update manual filtering code

---
+++ data/ps/filter.py
@@ -0,0 +1,50 @@
+import argparse
+import os
+import sys
+from pathlib import Path
+import random
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="filter data manually")
+    parser.add_argument("src_file", type=Path)
+    parser.add_argument("trg_file", type=Path)
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    src_file = args.src_file
+    trg_file = args.trg_file
+
+    with open(src_file, 'r') as f1:
+        src_data = f1.read()
+    with open(trg_file, 'r') as f2:
+        trg_data = f2.read()
+
+    # filter the src and target value by 20% manually
+    # assert len(src_data.split('\n')) == len(trg_data.split('\n'))
+    src_output = []
+    trg_output = []
+    for idx, item in enumerate(src_data.split('\n')):
+        if idx % 5 == 0:
+            continue # filter every fith line
+        else:
+            src_output.append(item)
+    for idx, item in enumerate(trg_data.split('\n')):
+        if idx % 5 == 0:
+            continue # filter every fith line
+        else:
+            trg_output.append(item)
+    temp = "\n".join(src_output)
+
+    # write into output file
+    with open('filter.ps', 'w') as f:
+        f.write("\n".join(src_output))
+    with open('filter.en', 'w') as f:
+        f.write("\n".join(trg_output))
+
+
+if __name__ == '__main__':
+    main()
